1d36eda3a3728de40ddbd620971b6e44ea656b87
Jayani Hewavitharana <jayani.hewa77@gmail.com>
2020-06-29 23:13:29 +0530

    supplier procurement dashboard logic

---
+++ src/api/routes/supplier.js
@@ -0,0 +1,90 @@
+const express = require("express");
+const bcrypt = require("bcrypt");
+
+const router = express.Router();
+const middlewares = require("../middlewares");
+
+// Database Models
+const supplierModel = require("../../models/supplier_model");
+
+module.exports = (app) => {
+  app.use("/supplier", router);
+
+  // ----------------------------------------------------------------------------------------
+  //                               Route Endpoints
+  // ----------------------------------------------------------------------------------------
+
+  // Health Check Route for Testing
+  router.get("", (req, res) => {
+    res.send("Ayubowan! from Test Routes, I'm Working").status(200).end();
+  });
+
+  router.get("/db", (req, res) => {
+    testModel.test().then((result) => {
+      // eslint-disable-next-line no-console
+      console.log(result);
+      res.json(result).status(200).end();
+    }).catch((err) => {
+      // eslint-disable-next-line no-console
+      console.log(err);
+      res.send(err).status(200).end();
+    });
+  });
+
+  router.post("/find_user", (req, res) => {
+    console.log(req.body);
+    userModel.findUserByEmailAndPassword(req.body.username, req.body.password).then((result) => {
+      res.json(result);
+    }).catch((err) => {
+      res.json(err);
+    });
+  });
+
+  router.post("/bcrypt/create_password_hash", async (req, res) => {
+  // Hash the password with a salt round of 10, the higher the rounds the more secure, but the slower
+  // your application becomes.
+    const hash = await bcrypt.hash(req.body.password, 10);
+    res.json({ hashed_password: hash });
+  });
+
+  //
+
+
+  router.get("/get_new_requests/", (req, res) => {
+    var supplier_id = req.query.id
+    console.log(supplier_id)
+    supplierModel.getNewRequests(supplier_id).then((result) => {
+        console.log('server', result)
+        //console.log(JSON.parse(result[0]['products']))
+        res.json(result);
+    }).catch((err) => {
+        res.json(err);
+    })
+    //res.send("get rfgs")
+  });
+
+  router.get("/get_ongoing_procurements/", (req, res) => {
+    var supplier_id = req.query.id
+    console.log(supplier_id)
+    supplierModel.getOngoingProcurements(supplier_id).then((result) => {
+        console.log('server', result)
+        //console.log(JSON.parse(result[0]['products']))
+        res.json(result);
+    }).catch((err) => {
+        res.json(err);
+    })
+  });
+
+  router.get("/get_completed_procurements/", (req, res) => {
+    var supplier_id = req.query.id
+    console.log(supplier_id)
+    supplierModel.getCompletedProcurements(supplier_id).then((result) => {
+        console.log('server', result)
+        //console.log(JSON.parse(result[0]['products']))
+        res.json(result);
+    }).catch((err) => {
+        res.json(err);
+    })
+  });
+
+};
---
+++ src/models/supplier_model.js
@@ -0,0 +1,86 @@
+const db = require("./mysql").pool;
+
+const getNewRequests = (supplier_id) => new Promise((resolve, reject) => {
+    db.getConnection((err, connection) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+  
+      // SQL Query
+      const sqlQueryString = `SELECT rfq.*, procurement.procurement_id, procurement.category, CONCAT('[',GROUP_CONCAT(CONCAT('{"product_id":"', rfq_product.product_id,'", "product_name":"', product.product_name, ' ", "qty":"', rfq_product.quantity,'"}')), ']') AS products 
+        FROM rfq 
+        INNER JOIN procurement ON rfq.procurement_id = procurement.procurement_id 
+        INNER JOIN rfq_product ON rfq.rfq_id = rfq_product.rfq_id 
+        INNER JOIN product ON rfq_product.product_id = product.product_id 
+        WHERE rfq.supplier_id='${supplier_id}' AND rfq.status='sent'
+        GROUP BY rfq.rfq_id`;
+      db.query(sqlQueryString, (error, results, fields) => {
+        // Release SQL Connection Back to the Connection Pool
+        connection.release();
+        //console.log(results)
+        resolve(JSON.parse(JSON.stringify(results)));
+      });
+    });
+});
+
+const getOngoingProcurements = (supplier_id) => new Promise((resolve, reject) => {
+  db.getConnection((err, connection) => {
+    if (err) {
+      reject(err);
+      return;
+    }
+
+    // SQL Query
+    const sqlQueryString = `SELECT DISTINCT
+        rfq.*, procurement.procurement_id, procurement.category, procurement.status AS procurement_status, procurement.bid_opening_date, bid.quotation, bid.status AS bid_status,
+        CONCAT('[',GROUP_CONCAT(CONCAT('{"product_id":"', bid_product.product_id,'", "product_name":"', product.product_name, ' ", "qty":"', bid_product.quantity, '", "price":"', bid_product.price,'"}')), ']') AS bids
+        FROM rfq 
+        INNER JOIN procurement ON rfq.procurement_id = procurement.procurement_id 
+        INNER JOIN bid ON procurement.procurement_id = bid.procurement_id
+        INNER JOIN bid_product ON bid.bid_id = bid_product.bid_id
+        INNER JOIN product ON bid_product.product_id = product.product_id
+        WHERE rfq.supplier_id='${supplier_id}' AND rfq.status='accepted' AND procurement.status='on-going'
+        GROUP BY bid.bid_id`;
+    db.query(sqlQueryString, (error, results, fields) => {
+      // Release SQL Connection Back to the Connection Pool
+      connection.release();
+      //console.log(results)
+      resolve(JSON.parse(JSON.stringify(results)));
+    });
+  });
+});
+
+const getCompletedProcurements = (supplier_id) => new Promise((resolve, reject) => {
+  db.getConnection((err, connection) => {
+    if (err) {
+      reject(err);
+      return;
+    }
+
+    // SQL Query
+    const sqlQueryString = `SELECT DISTINCT
+        rfq.*, procurement.procurement_id, procurement.category, procurement.status AS procurement_status, procurement.bid_opening_date, procurement.completed_date, bid.quotation, bid.status AS bid_status,
+        CONCAT('[',GROUP_CONCAT(CONCAT('{"product_id":"', bid_product.product_id,'", "product_name":"', product.product_name, ' ", "qty":"', bid_product.quantity, '", "price":"', bid_product.price,'"}')), ']') AS bids
+        FROM rfq 
+        INNER JOIN procurement ON rfq.procurement_id = procurement.procurement_id 
+        INNER JOIN bid ON procurement.procurement_id = bid.procurement_id
+        INNER JOIN bid_product ON bid.bid_id = bid_product.bid_id
+        INNER JOIN product ON bid_product.product_id = product.product_id
+        WHERE rfq.supplier_id='${supplier_id}' AND rfq.status='accepted' AND procurement.status='completed'
+        GROUP BY bid.bid_id`;
+    db.query(sqlQueryString, (error, results, fields) => {
+      // Release SQL Connection Back to the Connection Pool
+      connection.release();
+      //console.log(results)
+      resolve(JSON.parse(JSON.stringify(results)));
+    });
+  });
+});
+
+module.exports = {
+    getNewRequests,
+    getOngoingProcurements,
+    getCompletedProcurements
+};
+  
