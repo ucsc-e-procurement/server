e80a93c34274b2e209de0f9c4fa9bbce8c9f541c
Tharinda Dilshan Piyadasa <tharindad7@gmail.com>
2020-06-29 12:16:31 +0530

    Director | Model methods, Routes

--- src/api/routes/director_routes.js
+++ src/api/routes/director_routes.js
@@ -15,8 +15,128 @@ module.exports = (app) => {
 
   // Health Check Route for Testing
   router.get("", (req, res) => {
-    res.send("Ayubowan! from Director Routes, I'm Working").status(200).end();
+    res.send("Fuck Yeah from Director Routes, I'm Working").status(200).end();
   });
+
+
+  // Get Procurements
+  router.get("/procurements", (req, res) => {
+
+    // res.send("Getting procurements").status(200).end();
+
+    directorModel.getProcurements().then((result) => {
+      // eslint-disable-next-line no-console
+      console.log(result);
+      res.json(result).status(200).end();
+    }).catch((err) => {
+      // eslint-disable-next-line no-console
+      console.log(err);
+      res.send(err).status(200).end();
+    });
+  })
+
+  // Get Requests
+  router.get("/requests", (req, res) => {
+    
+    // res.send("Getting Requests").status(200).end();
+
+    directorModel.getRequisitionRequests().then((result) => {
+      // eslint-disable-next-line no-console
+      console.log(result);
+      res.json(result).status(200).end();
+    }).catch((err) => {
+      // eslint-disable-next-line no-console
+      console.log(err);
+      res.send(err).status(200).end();
+    });
+  })
+
+  // Get Product Requisition
+  router.post("/requisitions/:id", (req, res) => {
+
+    // res.send("Getting Requisition" + req.params.id).status(200).end();
+
+    directorModel.getRequisition(req.body.id).then((result) => {
+      // eslint-disable-next-line no-console
+      console.log(result);
+      res.json(result).status(200).end();
+    }).catch((err) => {
+      // eslint-disable-next-line no-console
+      console.log(err);
+      res.send(err).status(200).end();
+    });
+  })
+
+  // Get Procurement
+  router.post("/procurements/:id", (req, res) => {
+
+    // res.send("Getting procurement" + req.params.id).status(200).end();
+
+    directorModel.getProcurement(req.body.id).then((result) => {
+      // eslint-disable-next-line no-console
+      console.log(result);
+      res.json(result).status(200).end();
+    }).catch((err) => {
+      // eslint-disable-next-line no-console
+      console.log(err);
+      res.send(err).status(200).end();
+    });
+  })
+
+  // Approve requisition
+  router.post("/requisitions/:id/approve", (req, res) => {
+
+    // res.send("Approve Requisition " + req.params.id + " :" + req.body)
+
+    directorModel.approveRequisition(req.body.id, req.body.remarks, req.body.directorId).then((result) => {
+      // eslint-disable-next-line no-console
+      console.log(result);
+      res.json(result).status(200).end();
+    }).catch((err) => {
+      // eslint-disable-next-line no-console
+      console.log(err);
+      res.send(err).status(200).end();
+    });
+  })
+
+  // Appoint tech team
+  router.post("/procurements/:id/appointTechTeam", (req, res) => {
+
+    // res.send("Appointing tech team for procurement " + req.params.id + ": " + req.body).status(200).end();
+
+    directorModel.appointTechTeam(req.body.techTeamId, req.body.procurementId, req.body.directorId, req.body.employees).then((result) => {
+      // eslint-disable-next-line no-console
+      console.log(result);
+      res.json(result).status(200).end();
+    }).catch((err) => {
+      // eslint-disable-next-line no-console
+      console.log(err);
+      res.send(err).status(200).end();
+    });
+
+  })
+
+  // Appoint bid opening team
+  router.post("/procurements/:id/appointBidOpeningTeam", (req, res) => {
+
+    // res.send("Appointing bid opening team for procurement " + req.params.id + ": " + req.body).status(200).end();
+
+    directorModel.appointBidOpeningTeam(req.body.bidOpeningTeamId, req.body.directorId, req.body.member1, req.body.member2).then((result) => {
+      // eslint-disable-next-line no-console
+      console.log(result);
+      res.json(result).status(200).end();
+    }).catch((err) => {
+      // eslint-disable-next-line no-console
+      console.log(err);
+      res.send(err).status(200).end();
+    });
+
+  })
+
+  // Approve PO generation
+  router.post("/procurements/:id", (req, res) => {
+    res.send("Approve PO generation for procurement " + req.params.id + ": " + req.body).status(200).end();
+  })
 
 //   router.get("/db", (req, res) => {
 //     testModel.test().then((result) => {
--- src/models/director_model.js
+++ src/models/director_model.js
@@ -1,16 +1,143 @@
-const db = require("./mysql").connection;
+const db = require("./mysql").pool;
 
-// const director = () => new Promise((resolve, reject) => {
-//   db.connect((errDB) => {
-//     if (errDB) reject(errDB);
-//     db.query("SELECT * FROM test", (errQuery, result) => {
-//       if (errQuery) reject(errQuery);
-//       resolve(result);
-//     });
-//     db.end();
-//   });
-// });
+// Get procurements
+const getProcurements = () => new Promise((resolve, reject) => {
+    db.getConnection((err, connection) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+  
+      // SQL Query
+      const sqlQueryString = `SELECT * FROM procurement`;
+      db.query(sqlQueryString, (error, results, fields) => {
+        // Release SQL Connection Back to the Connection Pool
+        console.log(sqlQueryString, results, fields);
+        resolve(JSON.parse(JSON.stringify(results)));
+      });
+    });
+});
+
+// Get Product Requisition Requests
+const getRequisitionRequests = () => new Promise((resolve, reject) => {
+    db.getConnection((err, connection) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+  
+      // SQL Query
+      const sqlQueryString = `SELECT * FROM product_requisition WHERE director_id IS NULL AND deputy_bursar_id IS NOT NULL`;
+      db.query(sqlQueryString, (error, results, fields) => {
+        // Release SQL Connection Back to the Connection Pool
+        console.log(sqlQueryString, results, fields);
+        resolve(JSON.parse(JSON.stringify(results)));
+      });
+    });
+});
+
+// Get Product Requisition
+const getRequisition = (reqId, status = true) => new Promise((resolve, reject) => {
+    db.getConnection((err, connection) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+  
+      // SQL Query
+      const sqlQueryString = `SELECT * FROM product_requisition WHERE requisition_id = '${reqId}'`;
+      db.query(sqlQueryString, (error, results, fields) => {
+        // Release SQL Connection Back to the Connection Pool
+        console.log(sqlQueryString, results, fields);
+        resolve(JSON.parse(JSON.stringify(results)));
+      });
+    });
+});
+
+// Get Procurement*
+const getProcurement = (reqId, status = true) => new Promise((resolve, reject) => {
+    db.getConnection((err, connection) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+  
+      // SQL Query
+      const sqlQueryString = `SELECT * FROM procurement WHERE procurement_id = '${reqId}'`;
+      db.query(sqlQueryString, (error, results, fields) => {
+        // Release SQL Connection Back to the Connection Pool
+        console.log(sqlQueryString, results, fields);
+        resolve(JSON.parse(JSON.stringify(results)));
+      });
+    });
+});
+
+// Approve Product Requisition
+const approveRequisition = (reqId, remarks, directorId, status = true) => new Promise((resolve, reject) => {
+    db.getConnection((err, connection) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+  
+      // SQL Query
+      const sqlQueryString = `UPDATE product_requisition SET director_remarks = '${remarks}', director_id = '${directorId}' WHERE requisition_id = '${reqId}'`;
+      db.query(sqlQueryString, (error, results, fields) => {
+        // Release SQL Connection Back to the Connection Pool
+        console.log(sqlQueryString, results, fields);
+        resolve(JSON.parse(JSON.stringify(results)));
+      });
+    });
+});
+
+// Appoint Tech Team*
+const appointTechTeam = (techTeamId, procurementId, directorId, employees, status = true) => new Promise((resolve, reject) => {
+    db.getConnection((err, connection) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+
+      var date = new Date()
+  
+      // SQL Query
+      const sqlQueryString = `INSERT INTO tec_team VALUES ('${techTeamId}', '${date}', '${directorId}')`;
+      db.query(sqlQueryString, (error, results, fields) => {
+        // Release SQL Connection Back to the Connection Pool
+        console.log(sqlQueryString, results, fields);
+        resolve(JSON.parse(JSON.stringify(results)));
+      });
+    });
+});
+
+// Appoint Bid Opening Team*
+const appointBidOpeningTeam = (bidOpeningTeamId, directorId, member1, member2, status = true) => new Promise((resolve, reject) => {
+  db.getConnection((err, connection) => {
+    if (err) {
+      reject(err);
+      return;
+    }
+
+    var date = new Date()
+
+    // SQL Query
+    const sqlQueryString = `INSERT INTO bid_opening_team VALUES('${bidOpeningTeamId}', '2020-02-20', '${member1}', '${member2}', '${directorId}');`;
+    
+    db.query(sqlQueryString, (error, results, fields) => {
+      // Release SQL Connection Back to the Connection Pool
+      console.log(sqlQueryString, results, fields);
+      connection.release();
+      resolve(JSON.parse(JSON.stringify(results)));
+    });
+  });
+});
 
 module.exports = {
-
+    getProcurements,
+    getRequisitionRequests,
+    getRequisition,
+    getProcurement,
+    approveRequisition,
+    appointTechTeam,
+    appointBidOpeningTeam
 };
