0c583d148dedfca951611277bf8060a6f3f601cf
PasinduDew <pasindudewapriya96@gmail.com>
2020-08-29 12:30:03 +0530

    Added Firebase to the Server

---
+++ src/config/firebase_config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apiKey: "AIzaSyCViZT2qSV1pHClsEv2BHTX7nBDMT9nAfA",
+  authDomain: "ucsc-e-procurement.firebaseapp.com",
+  databaseURL: "https://ucsc-e-procurement.firebaseio.com",
+  projectId: "ucsc-e-procurement",
+  storageBucket: "ucsc-e-procurement.appspot.com",
+  messagingSenderId: "710042048062",
+  appId: "1:710042048062:web:c384fa29866f8061e3636f",
+  measurementId: "G-MJX7TX5LPT"
+};
--- src/loaders/index.js
+++ src/loaders/index.js
@@ -1,6 +1,8 @@
 const expressLoader = require("./express");
+const firebaseInitializer = require("./firebase");
 
 module.exports = async (expressApp) => {
   // Loading Express
   await expressLoader(expressApp);
+  await firebaseInitializer();
 };
