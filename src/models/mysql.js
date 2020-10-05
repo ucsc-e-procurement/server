32b18d1c8c5ce1df37ae9106dbed0c06ff8fb067
Prashastha Mudannayake <prashasthamudannayake98@gmail.com>
2020-10-05 07:35:38 +0530

    Spec sheet employee data get module

--- src/models/mysql.js
+++ src/models/mysql.js
@@ -10,18 +10,18 @@ const config = require("../config");
 // });
 
 const connection = mysql.createConnection({
-  host: "localhost",
+  host: "testdb-1.cezlzfckdvnf.ap-south-1.rds.amazonaws.com",
   user: "root",
-  password: "",
-  database: "UCSC_E_PROC",
+  password: "ucsc1234",
+  database: "ucsc_e_proc",
 });
 
 // If Connection Pooling is needed, use this approach instead
 const pool = mysql.createPool({
-  host: "localhost",
+  host: "testdb-1.cezlzfckdvnf.ap-south-1.rds.amazonaws.com",
   user: "root",
-  password: "",
-  database: "eprocnew",
+  password: "ucsc1234",
+  database: "ucsc_e_proc",
   connectionLimit: 100,
 });
 
